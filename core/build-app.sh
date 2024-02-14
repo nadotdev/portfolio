35a3d5502348b8d86f10c888134b5e4c67bb5da4
Ubuntu <ubuntu@ip-172-31-28-250.ap-southeast-1.compute.internal>
2024-02-14 06:37:56 +0000

    Change docker port to 80

--- core/build-app.sh
+++ core/build-app.sh
@@ -1,4 +1,4 @@
 sudo docker stop personal-portfolio-container
 sudo docker rm personal-portfolio-container
 sudo docker build . -t 'personal-portfolio-image:2024'
-sudo docker run --name personal-portfolio-container -d -it -p 9090:8000 --mount type=bind,source="$(pwd)",target=/core personal-portfolio-image:2024
+sudo docker run --name personal-portfolio-container -d -it -p 80:8000 --mount type=bind,source="$(pwd)",target=/core personal-portfolio-image:2024
