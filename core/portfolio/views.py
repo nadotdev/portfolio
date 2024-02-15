aff7583aba80a0c74988ed0c3d11917d8c0d9434
nadotdev <nadotdev@example.com>
2024-02-15 13:15:25 +0700

    add form send to telegram

--- core/portfolio/views.py
+++ core/portfolio/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+import requests
+import urllib.parse
 
-# Create your views here.
 
+TOKEN = "6728604177:AAHzoNld4gEaTg0AXAYGes3C9vE4RPbpk48"
+chat_id = "@cloudnaxyz"
 
 def home(request):
+    if request.method == "POST":
+        subject = request.POST.get('message_subject')
+        email = request.POST.get('email')
+        comment = request.POST.get('comment')
+
+        text = f"<strong>Subject:</strong> {subject}\n<b>Email:</b> {email}\n<b>Messages:</b> {comment}"
+        encoded_text = urllib.parse.quote(text)
+
+        # Construct the Telegram API URL
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&parse_mode=HTML&text={encoded_text}"
+        requests.get(url).json() # sends the message trigger
+        if HttpResponse.status_code == 200:
+            return render(request, 'portfolio/thank.html')
+
     return render(request, "portfolio/index.html")
